c7be726fe33c2bd7e16d37567aa17158a7376e95
Sergey <sergey.sav.26666@mail.ru>
2023-05-14 17:58:19 +0300

    Готово все, кроме адаптива

--- main.js
+++ main.js
@@ -3,37 +3,38 @@ const constructModal = document.querySelector('#construct-modal');
 const cross = document.querySelector('#cross');
 const closeBtn = document.querySelector('.modal__btn-close');
 const textarea = document.querySelector('.modal__textarea');
-
 const themeBtn = document.querySelector('.nav__theme');
 
-let darkTheme = false;
+// Тема
+let isDarkTheme = false;
 
+// Переменные для вывода результата submit формы
 let orderName = '';
-let sumPrice = 0;
+let orderSum = 0;
 let orderPrice = 0;
 let orderColor = '';
 let orderCount = 1;
 
-themeBtn.addEventListener('click', (e) => {
-    const themeElements = document.querySelectorAll('.theme');
-    const cross = document.querySelector('.modal__cross');
+/**
+ * Формат даты.
+ * @param {string} date - Дата
+ */
+const getDayInfo = (date) => new Date(date).getFullYear();
 
-    if (e.currentTarget.src.includes('sun')) {
-        e.currentTarget.src = './img/moon24.svg';
-        cross.src = './img/cross-white24.svg';
-        darkTheme = true;
-    } else {
-        e.currentTarget.src = './img/sun24.svg';
-        cross.src = './img/cross24.svg';
-        darkTheme = false;
-    }
+/**
+ * Формат цены.
+ * @param {number} money - Цена
+ */
+const getMoney = (money) =>
+    String(money)
+        .split(/(?=(?:\d{3})+(?!\d))/g)
+        .join(' ') + ' ₽';
 
-    themeElements.forEach((el) => {
-        el.classList.toggle('theme-dark');
-    });
-});
-
-const modalClose = (e) => {
+/**
+ * Эвент закрытия модального окна.
+ * @param {Event} e - Эвент
+ */
+const handleModalClose = (e) => {
     if (e.currentTarget === e.target) {
         modal.classList.remove('modal--active');
 
@@ -45,32 +46,20 @@ const modalClose = (e) => {
     }
 };
 
-modal.addEventListener('click', (e) => {
-    modalClose(e);
-});
-
-cross.addEventListener('click', (e) => {
-    modalClose(e);
-});
-
-closeBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    modalClose(e);
-});
-
+/**
+ * Эвент открытия модального окна.
+ * @param {Event} item - Товар
+ */
 const handleModalOpen = (item) => {
     modal.classList.add('modal--active');
 
     createModalItem(item);
 };
 
-const getDayInfo = (date) => new Date(date).getFullYear();
-
-const getMoney = (money) =>
-    String(money)
-        .split(/(?=(?:\d{3})+(?!\d))/g)
-        .join(' ') + ' ₽';
-
+/**
+ * Эвент нажатия на выбор цвета.
+ * @param {Event} e - Эвент
+ */
 const handleRadioClick = (e) => {
     const radio = document.querySelectorAll('.modal__fake');
 
@@ -84,6 +73,11 @@ const handleRadioClick = (e) => {
     orderColor = e.target.previousSibling.value;
 };
 
+/**
+ * Эвент нажатия на счетчик в форме.
+ * @param {Event} e - Эвент
+ * @param {HTMLElement} price - Элемент цены
+ */
 const handleCounterClick = (e, price) => {
     e.preventDefault();
 
@@ -102,104 +96,51 @@ const handleCounterClick = (e, price) => {
     }
     price.innerText = getMoney(orderPrice * +input.value);
     orderCount = input.value;
-    sumPrice = orderPrice * +input.value;
+    orderSum = orderPrice * +input.value;
 };
 
 /**
- * Функция для создания элемента с классом и текстом
- *
- * @param {keyof HTMLElementTagNameMap} element - HTML Элемент
- * @param {string | string[]} style - Класс
- * @param {string} [text] - Текст
- * @param {boolean} [isDarkTheme] - Создавать стиль для темной темы
+ * Функия создания элемента с стилями и атрибутами.
+ * @param {keyof HTMLElementTagNameMap} element - Элемент
+ * @param {Array<string>} styles - Список стилей
+ * @param {{[x: key]: string}} attr - Список атрибутов
  */
-const createCustomElement = (element, style, text, isDarkTheme) => {
+const createCustomElement = (element, styles, attr) => {
     const el = document.createElement(`${element}`);
 
-    if (Array.isArray(style)) {
-        style.forEach((cls) => {
-            el.classList.add(`${cls}`);
-        });
-    } else {
-        el.classList.add(`${style}`);
-    }
+    styles.forEach((cls) => el.classList.add(`${cls}`));
 
-    if (isDarkTheme && darkTheme) {
-        el.classList.add('theme-dark');
-    }
-
-    if (text) {
-        el.innerText = text;
+    for (let key in attr) {
+        switch (key) {
+            case 'theme':
+                if (isDarkTheme) {
+                    el.classList.add('theme-dark');
+                }
+                break;
+            case 'text':
+                el.innerText = attr[key];
+                break;
+            default:
+                el.setAttribute(key, attr[key]);
+        }
     }
 
     return el;
 };
 
-const createModalItem = (item) => {
-    const imgWrapper = createCustomElement('div', 'modal__img-wrapper');
-    const img = createCustomElement('img', 'modal__img');
-    const textWrapper = createCustomElement('div', 'modal__product-text-wrapper');
-    const textName = createCustomElement('p', 'modal__product-text', 'Наименование товара');
-    const textColor = createCustomElement('p', 'modal__product-text', 'Цвет');
-    const textCounter = createCustomElement('p', 'modal__product-text', 'Количество');
-    const textPrice = createCustomElement('p', 'modal__product-text', 'Цена');
-    const name = createCustomElement('p', 'modal__product-name', '', true);
-    const inputWrapper = createCustomElement('div', 'modal__input-wrapper');
-    const inputTable = createCustomElement('div', 'modal__input-table');
-    const pointerWrapper = createCustomElement('div', 'modal__pointer-wrapper');
-    const priceWrapper = createCustomElement('div', 'modal__price-wrapper');
-
-    const counter = createCustomElement('div', 'counter');
-    const counterMinus = createCustomElement('button', 'counter__minus', '', true);
-    const counterPlus = createCustomElement('button', 'counter__plus', '', true);
-    const counterMinusImg = createCustomElement('img', 'counter__minus-img');
-    const counterPlusImg = createCustomElement('img', 'counter__plus-img');
-    const counterInput = createCustomElement('input', 'counter__input', '', true);
-
-    const price = createCustomElement('p', 'modal__price', '', true);
-
-    // Атрибуты из ITEM
-    img.src = item.img;
-    img.alt = 'Картинка товара';
-    name.innerText = item.name;
-    orderPrice = item.cost;
-    sumPrice = item.cost;
-    price.innerText = getMoney(orderPrice);
-
-    // Атрибуты для COUNTER INPUT
-    counterInput.disabled = true;
-    counterInput.value = '1';
-    counterInput.name = 'counter';
-    counterInput.type = 'text';
-    if (darkTheme) {
-        counterMinusImg.src = './img/minus-white12.svg';
-        counterPlusImg.src = './img/plus-white12.svg';
-    } else {
-        counterMinusImg.src = './img/minus12.svg';
-        counterPlusImg.src = './img/plus12.svg';
-    }
-    counterMinus.value = 'minus';
-    counterPlus.value = 'plus';
-
-    orderName = item.name;
-
-    // Генерация
-    constructModal.append(imgWrapper);
-    imgWrapper.append(img);
-    imgWrapper.after(textWrapper);
-    textWrapper.append(textName);
-    textName.after(name);
-    textWrapper.after(inputWrapper);
-    inputWrapper.append(textColor);
-    textColor.after(inputTable);
-
-    Object.keys(item.color).forEach((color, i) => {
-        const inputStyle = createCustomElement('label', 'modal__input-style');
-        const radioInput = createCustomElement('input', 'modal__input');
-        const radioStyle = createCustomElement('span', 'modal__fake');
-        radioInput.type = 'radio';
-        radioInput.name = 'color';
-        radioInput.value = color;
+/**
+ * Функция создания списка элементов для цветов.
+ * @param {{[x: colorName]: color}} colors - Объект цветов товара
+ */
+const createColorElements = (colors) => {
+    return Object.keys(colors).map((color, i) => {
+        const inputStyle = createCustomElement('label', ['modal__input-style']);
+        const radioInput = createCustomElement('input', ['modal__input'], {
+            type: 'radio',
+            name: 'color',
+            value: color,
+        });
+        const radioStyle = createCustomElement('span', ['modal__fake']);
 
         radioStyle.addEventListener('click', handleRadioClick);
 
@@ -207,86 +148,173 @@ const createModalItem = (item) => {
             radioInput.checked = true;
             radioStyle.classList.add('modal__fake--active');
         }
-        radioStyle.style = `background-color: ${item.color[color]}`;
+        radioStyle.style = `background-color: ${colors[color]}`;
 
-        inputTable.append(inputStyle);
-        inputStyle.append(radioInput);
-        inputStyle.append(radioStyle);
+        return [inputStyle, radioInput, radioStyle];
     });
+};
 
-    inputWrapper.after(pointerWrapper);
-    pointerWrapper.append(textCounter);
-    pointerWrapper.append(counter);
-    counter.append(counterMinus);
-    counterMinus.append(counterMinusImg);
-    counter.append(counterInput);
-    counter.append(counterPlus);
-    counterPlus.append(counterPlusImg);
+/**
+ * Создание товара в модальном окне.
+ * @param {{name: string; cost: number; img: string; color: {[x: colorName]: color}}} item - Объект товара
+ */
+const createModalItem = (item) => {
+    // Инициализация переменных товара
+    orderPrice = item.cost;
+    orderSum = item.cost;
+    orderName = item.name;
 
-    pointerWrapper.after(priceWrapper);
-    priceWrapper.append(textPrice);
-    priceWrapper.append(price);
+    // Инициализация блоков
+    const imgWrapper = createCustomElement('div', ['modal__img-wrapper']);
+    const img = createCustomElement('img', ['modal__img'], { src: item.img, alt: 'Картинка товара' });
+    const textWrapper = createCustomElement('div', ['modal__product-text-wrapper']);
+    const textName = createCustomElement('p', ['modal__product-text'], { text: 'Наименование товара' });
+    const textColor = createCustomElement('p', ['modal__product-text'], { text: 'Цвет' });
+    const textCounter = createCustomElement('p', ['modal__product-text'], { text: 'Количество' });
+    const textPrice = createCustomElement('p', ['modal__product-text'], { text: 'Цена' });
+    const name = createCustomElement('p', ['modal__product-name'], { theme: true, text: item.name });
+    const inputWrapper = createCustomElement('div', ['modal__input-wrapper']);
+    const inputTable = createCustomElement('div', ['modal__input-table']);
+    const pointerWrapper = createCustomElement('div', ['modal__pointer-wrapper']);
+    const priceWrapper = createCustomElement('div', ['modal__price-wrapper']);
+    const counter = createCustomElement('div', ['counter']);
+    const counterMinus = createCustomElement('button', ['counter__minus'], { value: 'minus', theme: true });
+    const counterPlus = createCustomElement('button', ['counter__plus'], { value: 'plus', theme: true });
+    const counterMinusImg = createCustomElement('img', ['counter__minus-img'], {
+        src: isDarkTheme ? './img/minus-white12.svg' : './img/minus12.svg',
+    });
+    const counterPlusImg = createCustomElement('img', ['counter__plus-img'], {
+        src: isDarkTheme ? './img/plus-white12.svg' : './img/plus12.svg',
+    });
+    const counterInput = createCustomElement('input', ['counter__input'], {
+        theme: true,
+        disabled: true,
+        value: '1',
+        name: 'counter',
+        type: 'text',
+    });
+    const price = createCustomElement('p', ['modal__price'], { theme: true, text: getMoney(orderPrice) });
 
+    // Эвенты счетчика
     counterMinus.addEventListener('click', (e) => handleCounterClick(e, price));
     counterPlus.addEventListener('click', (e) => handleCounterClick(e, price));
+
+    // Генерация в HTML
+    constructModal.append(imgWrapper, textWrapper, inputWrapper, pointerWrapper, priceWrapper);
+    imgWrapper.append(img);
+
+    textWrapper.append(textName, name);
+
+    inputWrapper.append(textColor, inputTable);
+    createColorElements(item.color).forEach((color) => {
+        inputTable.append(...color);
+    });
+
+    pointerWrapper.append(textCounter, counter);
+    counter.append(counterMinus, counterInput, counterPlus);
+    counterMinus.append(counterMinusImg);
+    counterPlus.append(counterPlusImg);
+
+    priceWrapper.append(textPrice, price);
 };
 
+/**
+ * Создание элементов товаров на главной странице.
+ * @param {{name: string; cost: number; date: string; img: string}} item - Объект товара
+ * @param {HTMLElement} parant - Родительский элемент для вставки в HTML
+ */
 const createCategoryItem = (item, parent) => {
-    //Инициализация блоков
-    const categoryItem = createCustomElement('li', 'category__item');
-    const imgWrapper = createCustomElement('div', 'category__img-wrapper');
-    const img = createCustomElement('img', 'category__img');
-    const date = createCustomElement('p', 'category__date');
-    const cost = createCustomElement('p', ['category__cost', 'theme']);
-    const name = createCustomElement('p', ['category__name', 'theme']);
-    const btn = createCustomElement('button', 'category__btn');
+    // Инициализация блоков
+    const categoryItem = createCustomElement('li', ['category__item']);
+    const imgWrapper = createCustomElement('div', ['category__img-wrapper']);
+    const img = createCustomElement('img', ['category__img'], { src: item.img, alt: 'Картинка товара' });
+    const date = createCustomElement('p', ['category__date'], { text: getDayInfo(item.date) });
+    const cost = createCustomElement('p', ['category__cost', 'theme'], { text: getMoney(item.cost) });
+    const name = createCustomElement('p', ['category__name', 'theme'], { text: item.name });
+    const btn = createCustomElement('button', ['category__btn'], { text: 'Купить' });
 
-    //Инициализация контента блоков
-    date.innerText = getDayInfo(item.date);
-    name.innerText = item.name;
-    cost.innerText = getMoney(item.cost);
-    btn.innerText = 'Купить';
-    img.src = item.img;
-    img.alt = 'Картинка товара';
-
-    //Работа с кнопкой
+    // Эвент кнопки купить
     btn.addEventListener('click', () => handleModalOpen(item));
 
-    //Вставка в HTML документ
+    // Генерация в HTML
     parent.append(categoryItem);
-    categoryItem.append(imgWrapper);
+    categoryItem.append(imgWrapper, date, cost, name, btn);
     imgWrapper.append(img);
-    imgWrapper.after(date);
-    date.after(cost);
-    cost.after(name);
-    name.after(btn);
 };
 
-for (let key in initialData) {
-    const parent = document.querySelector(`#${key}`);
-    initialData[key].forEach((obj) => {
-        createCategoryItem(obj, parent);
+/**
+ * Инициализация submit для формы.
+ */
+const initFormAction = () => {
+    const form = document.querySelector('form');
+
+    form.addEventListener('submit', submitForm, true);
+
+    function submitForm(e) {
+        e.preventDefault();
+
+        const message = textarea.value;
+
+        alert(
+            `Мы приняли ваш заказ, спасибо
+            Название: ${orderName}
+            Цвет: ${orderColor}
+            Количество: ${orderCount}
+            Цена (шт.): ${getMoney(orderPrice)}
+            Общая цена: ${getMoney(orderSum)}
+            ${message && `Комментарий: ${message}`}
+            `
+        );
+        handleModalClose(e);
+    }
+};
+
+/**
+ * Входная точка в приложение.
+ * @param {{[x: key]: any}} data - Начальные данные из файла initialData.js
+ */
+const initApp = (data) => {
+    //Рендер товаров на главной
+    for (let key in data) {
+        const parent = document.querySelector(`#${key}`);
+        data[key].forEach((obj) => {
+            createCategoryItem(obj, parent);
+        });
+    }
+
+    // Эвенты
+    themeBtn.addEventListener('click', (e) => {
+        const themeElements = document.querySelectorAll('.theme');
+        const cross = document.querySelector('.modal__cross');
+
+        if (e.currentTarget.src.includes('sun')) {
+            e.currentTarget.src = './img/moon24.svg';
+            cross.src = './img/cross-white24.svg';
+            isDarkTheme = true;
+        } else {
+            e.currentTarget.src = './img/sun24.svg';
+            cross.src = './img/cross24.svg';
+            isDarkTheme = false;
+        }
+
+        themeElements.forEach((el) => {
+            el.classList.toggle('theme-dark');
+        });
     });
-}
+    modal.addEventListener('click', (e) => {
+        handleModalClose(e);
+    });
+    cross.addEventListener('click', (e) => {
+        handleModalClose(e);
+    });
+    closeBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        handleModalClose(e);
+    });
 
-const form = document.querySelector('form');
+    //Рендер формы
+    initFormAction();
+};
 
-form.addEventListener('submit', submitForm, true);
-
-function submitForm(e) {
-    e.preventDefault();
-
-    const message = textarea.value;
-
-    alert(
-        `Мы приняли ваш заказ, спасибо 
-        Название: ${orderName}
-        Цвет: ${orderColor}
-        Количество: ${orderCount}
-        Цена (шт.): ${getMoney(orderPrice)}
-        Общая цена: ${getMoney(sumPrice)}
-        ${message && `Комментарий: ${message}`}
-        `
-    );
-    modalClose(e);
-}
+// Начальный вызов приложения
+initApp(initialData);
